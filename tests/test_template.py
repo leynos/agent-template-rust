2f4a947b640561cf9a49a38f312b237958357fa2
Leynos <leynos@troubledskies.net>
2025-06-11 10:13:50 +0100

    Add templates suffix setting

--- tests/test_template.py
+++ tests/test_template.py
@@ -5,24 +5,21 @@ from datetime import datetime
 import subprocess
 
 import pytest
-from pytest_copier import CopierRunner
+from pytest_copier.plugin import CopierFixture
 
 TEMPLATE_PATH = Path(__file__).parents[1]
 
 
-def test_template_renders(tmp_path: Path) -> None:
+def test_template_renders(tmp_path: Path, copier: CopierFixture) -> None:
     """Template renders with default values."""
-    runner = CopierRunner(str(TEMPLATE_PATH))
-    project = runner.copy(
-        str(tmp_path),
-        data={
-            "project_name": "Example",
-            "package_name": "example",
-            "license_year": datetime.now().year,
-            "license_holder": "Example Dev",
-            "license_email": "example@example.com",
-        },
+    project = copier.copy(
+        tmp_path,
+        project_name="Example",
+        package_name="example",
+        license_year=datetime.now().year,
+        license_holder="Example Dev",
+        license_email="example@example.com",
     )
     assert (project / "Cargo.toml").exists()
     assert (project / "src" / "main.rs").exists()
-    subprocess.run(["cargo", "build"], cwd=project, check=True)
+    project.run("cargo build")
